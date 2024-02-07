cd16f380122ab043fa7b7782b1ae6cb615d23f7e
chelseyqc <6721@holbertonstudents.com>
2024-02-07 13:04:24 +0800

    feat: updated to landing page and spotify SSO

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+const qs = require('qs');
+
 const app = express();
 const port = 3000;
 
@@ -7,7 +11,20 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const spotifyAuthUrl = 'ADD_SPOTIFY_AUTH_URL';
+    const scopes = [
+        'user-read-private',
+        'user-library-read',
+        'user-read-recently-played',
+        'playlist-modify-public'
+    ].join(' ');
+
+    const spotifyAuthUrl = `https://accounts.spotify.com/authorize?${qs.stringify({
+        response_type: 'code',
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: scopes,
+        redirect_uri: process.env.REDIRECT_URI,
+        show_dialog: true,
+    })}`;
     res.render('login', { spotifyAuthUrl });
 });
 
