7611d7ae907db603cbf9202db824f0e3e3ad9c19
chelseyqc <6721@holbertonstudents.com>
2024-02-12 14:23:53 +0800

    feat: added callback

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const qs = require('qs');
+const User = require('./models/User');
+const { exchangeCodeForTokens, getUserInfo, calculateTokenExpiry } = require('./spotifyUtils');
 
 const app = express();
 const port = 3000;
@@ -26,6 +28,44 @@ app.get('/', (req, res) => {
         show_dialog: true,
     })}`;
     res.render('login', { spotifyAuthUrl });
+});
+
+app.get('/callback', async (req, res) => {
+    const code = req.query.code;
+    const spotifyTokens = await exchangeCodeForTokens(code);
+
+    const userInfo = await getUserInfo(spotifyTokens.access_token);
+
+    let user = await User.findOne({ spotifyId: userInfo.id });
+
+    // create a new user if they don't exist
+    if (!user) {
+        user = new User({
+            spotifyID: userInfo.id,
+            accessToken: spotifyTokens.access_token,
+            refreshToken: spotifyTokens.refresh_token,
+            tokenExpiry: calculateTokenExpiry(spotifyTokens.expires_in)
+        });
+        await user.save();
+    } else {
+        // update user's existing tokens
+        user.accessToken = spotifyTokens.access_token;
+        user.refreshToken = spotifyTokens.refresh_token;
+        user.tokenExpiry = calculateTokenExpiry(spotifyTokens.expires_in);
+        await user.save();
+    }
+    // not yet implemented, waiting on quiz schema
+    const userQuizCompleted = async (userId) => {
+        const quizCount = await Quiz.countDocuments({ userId: userId });
+        return quizCount > 0;
+    }
+    // not yet implemented, waiting on quiz schema
+    const hasCompletedQuiz = await userQuizCompleted(user._id);
+    if (hasCompletedQuiz) {
+        res.redirect('/home');
+    } else {
+        res.redirect('/quiz');
+    }
 });
 
 app.listen(port, () => {
