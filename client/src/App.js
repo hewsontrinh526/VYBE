7b025388f3f7b04eb66d05e3210e235ba1475172
alicia <6722@holbertonstudents.com>
2024-02-13 22:17:36 +1100

    feat: added route path for quiz

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import ColourQuiz from './components/ColourQuiz';
 
 function App() {
-  return  (
+  return (
     <Router>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/quiz" element={<ColourQuiz />} />
         {/* add other routes here */}
       </Routes>
     </Router>
