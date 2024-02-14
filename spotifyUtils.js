1de5b02f8427859560271bb84d52b8f6b7627b79
chelseyqc <6721@holbertonstudents.com>
2024-02-14 09:58:20 +0800

    fix: changed to https for tokenUrl

--- spotifyUtils.js
+++ spotifyUtils.js
@@ -1,12 +1,20 @@
+// http client for making requests
 const axios = require('axios');
+// querystring for parsing and stringifying
 const qs = require('qs');
 
+// exchange authorisation code for access and refresh tokens
 async function exchangeCodeForTokens(code) {
-  const tokenUrl = 'http://accounts.spotify.com/api/token';
+  // send POST request with data below
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
   const body = {
+    // requesting tokens with an auth code
     grant_type: 'authorization_code',
+    // auth code recieved
     code: code,
+    // redirect uri after auth (same as spotify dev dashboard)
     redirect_uri: process.env.REDIRECT_URI,
+    // creds from spotify
     client_id: process.env.SPOTIFY_CLIENT_ID,
     client_secret: process.env.SPOTIFY_CLIENT_SECRET,
   };
@@ -14,6 +22,7 @@ async function exchangeCodeForTokens(code) {
   try {
     const response = await axios.post(tokenUrl, qs.stringify(body), {
       headers: {
+        // request content required by spotify
         'Content-Type': 'application/x-www-form-urlencoded',
       },
     });
@@ -25,16 +34,20 @@ async function exchangeCodeForTokens(code) {
   }
 }
 
+// retrieves current user's info from spotify using access token
 async function getUserInfo(accessToken) {
+  // send a GET request
   const userUrl = 'https://api.spotify.com/v1/me';
 
   try {
     const response = await axios.get(userUrl, {
       headers: {
+        // authenticates the request with this header
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    // return the user info
+    // return the user info, e.g. spotify ID, display name, sub type, etc.
+    // depends on scopes included
     return response.data;
   } catch (error) {
     console.error('Error getting user info', error.response.data);
@@ -42,8 +55,12 @@ async function getUserInfo(accessToken) {
   }
 }
 
+// calculates expiry time of access token based on current time and "expires_in"
+// expires_in = lifespan of access token in seconds provided by spotify
 function calculateTokenExpiry(expiresIn) {
+  // retrieve current date and time
   const currentTime = new Date();
+  // add expires_in duration to current time. Converted to milliseconds
   const expiryTime = new Date(currentTime.getTime() + expiresIn * 1000);
   return expiryTime;
 }
