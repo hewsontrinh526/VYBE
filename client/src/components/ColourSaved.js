5583bb2bb9411f4308aa871ddf449ab995f196d2
aliciastudies <6722@holbertonstudents.com>
2024-02-15 21:19:42 +1100

    feat: added function to add and save selected colours in circles

---
+++ client/src/components/ColourSaved.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styles from './ColourQuiz.module.css';
+import './blobs.css';
+import ColourSelect from './ColourSelect';
+
+function ColourSaved({ newColour, clickCount }) {
+	const circleStyles = [
+		styles['circle-1'],
+		styles['circle-2'],
+		styles['circle-3'],
+		styles['circle-4'],
+		styles['circle-5'],
+	];
+
+	let circles = [];
+	for (let index = 0; index < 5; index++) {
+		const circleColour =
+			index + 1 === clickCount ? newColour : circleStyles[index];
+		circles.push(
+			<div
+				key={index}
+				className={styles[`circle-${index + 1}`]}
+				style={{ backgroundColor: circleColour }}
+			></div>
+		);
+	}
+
+	return <div className={styles['colours-saved']}>{circles}</div>;
+}
+
+export default ColourSaved;
