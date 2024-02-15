befd41aadf6df7901eb5fb74f60cd53e4a73d948
aliciastudies <6722@holbertonstudents.com>
2024-02-15 21:18:12 +1100

    refractor: Separated components into their own files

--- client/src/components/ColourQuiz.js
+++ client/src/components/ColourQuiz.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from './ColourQuiz.module.css';
 import './blobs.css';
+import ColourSelect from './ColourSelect';
+import Playlist from './Playlist';
+import ColourSaved from './ColourSaved';
 
 function ColourQuiz() {
-	const handleSaveColour = (event) => {
-		const selectedColour = event.target.getAttribute('data-colour');
-		console.log(selectedColour);
-	};
-
 	return (
 		<div className='quiz-container'>
 			<header className={styles['header']}>
@@ -15,49 +13,9 @@ function ColourQuiz() {
 					<img src='/logo.png' alt='logo' className='logo' />
 				</div>
 			</header>
-			<div className={styles['colours-select']}>
-				<button
-					className={styles['square-1']}
-					data-colour='#DF0000'
-					onClick={handleSaveColour}
-				></button>
-				<button
-					className={styles['square-2']}
-					data-colour='#F58501'
-					onClick={handleSaveColour}
-				></button>
-				<button
-					className={styles['square-3']}
-					data-colour='#FAFF01'
-					onClick={handleSaveColour}
-				></button>
-				<button
-					className={styles['square-4']}
-					data-colour='#5EF105'
-					onClick={handleSaveColour}
-				></button>
-				<button
-					className={styles['square-5']}
-					data-colour='#00B2FF'
-					onClick={handleSaveColour}
-				></button>
-				<button
-					className={styles['square-6']}
-					data-colour='#BD00FF'
-					onClick={handleSaveColour}
-				></button>
-			</div>
-			<div className={styles['playlist']}>
-				<h1 id='playlistName'>Song Name</h1>
-				<h2>Artist</h2>{' '}
-			</div>
-			<div className={styles['colours-saved']}>
-				<div className={styles['circle-1']}></div>
-				<div className={styles['circle-1']}></div>
-				<div className={styles['circle-1']}></div>
-				<div className={styles['circle-1']}></div>
-				<div className={styles['circle-1']}></div>
-			</div>
+			<ColourSelect />
+			<Playlist />
+			<ColourSaved />
 		</div>
 	);
 }
