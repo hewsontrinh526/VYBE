3f1c30207c83d5f498f758e4fc9bfb7ad5813b05
hewsontrinh526 <6734@holbertonstudents.com>
2024-02-16 12:57:17 +1100

    Feat: Updated files to handle mongoDB

--- server.js
+++ server.js
@@ -11,11 +11,17 @@ const cors = require('cors');
 
 // models and utility functions
 const User = require('./models/User');
+const Quiz = require('./models/Quiz');
 const { exchangeCodeForTokens, getUserInfo, calculateTokenExpiry } = require('./spotifyUtils');
 
 // init express app
 const app = express();
 const port = 3500;
+
+// connecting to mongoDB 
+mongoose.connect('mongodb://localhost:27017/VYBE')
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.error(err));
 
 // middlewares
 // for parsing application/json
@@ -47,7 +53,7 @@ app.get('/api/auth/url', (req, res) => {
 });
 
 // simple route to test the callback
-app.get('/callback', async (req, res) => {
+/* app.get('/callback', async (req, res) => {
     const code = req.query.code; // the code from spotify's redirect
 
     try {
@@ -59,9 +65,9 @@ app.get('/callback', async (req, res) => {
     } catch (error) {
         console.error('Error during code exchange:', error);
     }
-})
+}) */
 
-/* commenting out to test the simple callback above works
+// commenting out to test the simple callback above works
 // route for spotify callback
 app.get('/callback', async (req, res) => {
     // code from the query parameters
@@ -73,7 +79,7 @@ app.get('/callback', async (req, res) => {
     const userInfo = await getUserInfo(spotifyTokens.access_token);
 
     // find existing user in the database
-    let user = await User.findOne({ spotifyId: userInfo.id });
+    let user = await User.findOne({ spotifyID: userInfo.id });
 
     // create a new user if they don't already exist
     if (!user) {
@@ -85,6 +91,7 @@ app.get('/callback', async (req, res) => {
         });
         // save new user to db
         await user.save();
+        console.log('New user created:', user);
     } else {
         // update user's existing tokens
         user.accessToken = spotifyTokens.access_token;
@@ -92,9 +99,10 @@ app.get('/callback', async (req, res) => {
         user.tokenExpiry = calculateTokenExpiry(spotifyTokens.expires_in);
         // save updated user info
         await user.save();
+        console.log('User info updated:', user);
     }
     // not yet implemented, waiting on quiz schema
-    const userQuizCompleted = async (userId) => {
+    /* const userQuizCompleted = async (userId) => {
         const quizCount = await Quiz.countDocuments({ userId: userId });
         return quizCount > 0;
     }
@@ -104,9 +112,11 @@ app.get('/callback', async (req, res) => {
         res.redirect('/app/home');
     } else {
         res.redirect('/app/quiz');
-    }
+    } */
+
+    // redirect to /app/home for testing purposes
+    res.redirect('/app/home');
 });
-*/
 
 // fallback to serve react app for any other routes
 app.get('/app/*', (req, res) => {
