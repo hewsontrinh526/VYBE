e3e1a4290199cd35dd15eeb6db5959605b71fd7f
hewsontrinh526 <6734@holbertonstudents.com>
2024-02-21 14:53:26 +1100

    Fix: Added mongoose requirement and removed need for songID

--- models/Quiz.js
+++ models/Quiz.js
@@ -1,6 +1,9 @@
+const mongoose = require('mongoose');
+
 const quizSchema = new mongoose.Schema({
     spotifyID: { type: String, required: true, ref: 'User'},
     songs: [{
+        // songID: { type: String, required: true },
         colorValue: { type: String, required: true }, // HEX, RGB, or HSL value
         energy: { type: Number, required: true, min: 0, max: 1 },
         danceability: { type: Number, required: true, min: 0, max: 1 },
