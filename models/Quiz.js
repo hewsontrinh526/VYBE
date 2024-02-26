50b5ebcbda2d94973d81d81c464fce93bbce44cf
hewsontrinh526 <6734@holbertonstudents.com>
2024-02-26 17:48:30 +1100

    Feat: Uncommented redirect

--- models/Quiz.js
+++ models/Quiz.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 const quizSchema = new mongoose.Schema({
     spotifyID: { type: String, required: true, ref: 'User' },
     songs: [{
+        trackID: { type: String, required: true },
         colourHue: { type: Number, required: true, min: 0, max: 360 },
         colourSaturation: { type: Number, required: true, min: 0, max: 100 },
         colourLightness: { type: Number, required: true, min: 0, max: 100 },
-        energy: { type: Number, required: true, min: 0, max: 1 },
-        danceability: { type: Number, required: true, min: 0, max: 1 },
-        valence: { type: Number, required: true, min: 0, max: 1 },
     }]
 });
 
