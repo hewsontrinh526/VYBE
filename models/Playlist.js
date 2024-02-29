f5b49163dcb66e01c13a1860b585afdd53d25292
hewsontrinh526 <6734@holbertonstudents.com>
2024-03-01 10:40:38 +1100

    Feat: Added playlist schema

--- models/Playlist.js
+++ models/Playlist.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const playlistSchema = new mongoose.Schema({
     spotifyID: { type: String, required: true, ref: 'User' },
-    playlistID: [{ type: String, required: true}]
+    playlist: [{
+        playlistID: { type: String, required: true },
+        colourHue: { type: Number, required: true, min: 0, max: 360 },
+        colourSaturation: { type: Number, required: true, min: 0, max: 100 },
+        colourLightness: { type: Number, required: true, min: 0, max: 100 },
+    }]
 });
 
 const Playlist = mongoose.model('Playlist', playlistSchema);
